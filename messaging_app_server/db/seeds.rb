4f1537810703bcd6f106d8ca7944472cee4e8d88
MrazTevin <millatevin@gmail.com>
2024-02-09 19:36:24 +0300

    m: read data from the csv and add to the database, checking if a particular user exists

--- messaging_app_server/db/seeds.rb
+++ messaging_app_server/db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'csv'
+
+# Define the absolute path to your CSV file
+csv_file = File.expand_path('../../../../GeneralistRails_Project_MessageData.csv', __FILE__)
+
+# Read the CSV file and seed the database
+CSV.foreach(csv_file, headers: true) do |row|
+  # Extract data from each row of the CSV file
+  user_id = row['User ID']
+  timestamp = row['Timestamp (UTC)']
+  message_body = row['Message Body']
+
+  # Find the User record corresponding to the user_id from the CSV file
+  user = User.find_by(id: user_id)
+
+  if user.nil?
+    puts "User with ID #{user_id} not found. Skipping message creation."
+  else
+    # Create a Message associated with the found User
+    begin
+      Message.create!(user: user, timestamp: timestamp, message_body: message_body)
+    rescue ActiveRecord::RecordInvalid => e
+      puts "Failed to create message: #{e.record.errors.full_messages.join(", ")}"
+    end
+  end
+end
