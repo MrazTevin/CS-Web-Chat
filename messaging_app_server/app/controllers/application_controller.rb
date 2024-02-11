37df514bd2aca8e1aa93259afd9e810f8196ba82
MrazTevin <millatevin@gmail.com>
2024-02-11 06:45:20 +0300

    m: handle csrf token issues

--- messaging_app_server/app/controllers/application_controller.rb
+++ messaging_app_server/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def csrf_token
+        render json: { csrfToken: form_authenticity_token }
+      end
+    protect_from_forgery with: :exception, prepend: true
+    before_action :set_https_header
+
+    def set_https_header
+    response.set_header('X-Frame-Options', 'SAMEORIGIN')
+    end
 end
