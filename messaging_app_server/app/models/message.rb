7cbbcc7a9407f969cad7f85fcddf206f8065bccd
MrazTevin <millatevin@gmail.com>
2024-02-11 10:44:53 +0300

    m: rename the parameter

--- messaging_app_server/app/models/message.rb
+++ messaging_app_server/app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one :customer, through: :user
   validates :user_id, presence: true
-  validates :message, presence: true
+  validates :message_body, presence: true
 end
