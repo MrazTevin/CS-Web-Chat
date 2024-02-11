fbba2c4657a91f1090c48c1386beffa76e1088f1
MrazTevin <millatevin@gmail.com>
2024-02-11 06:46:55 +0300

    m: add method to grab the csrf

--- messaging_app_server/config/routes.rb
+++ messaging_app_server/config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get '/csrf-token', to: 'application#csrf'
 
   # Defines the root path route ("/")
   # root "posts#index"
