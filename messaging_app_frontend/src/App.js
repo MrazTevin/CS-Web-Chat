0c70d0181bf7a4fa58a5c67e41dde11c013e9341
MrazTevin <millatevin@gmail.com>
2024-02-11 15:49:07 +0300

    m: enable routing in the application

--- messaging_app_frontend/src/App.js
+++ messaging_app_frontend/src/App.js
@@ -1,23 +1,57 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import CustomerMessageForm from './components/CustomerMessageForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AgentMessageList from './components/AgentMessageList';
 import CustomerForm from './components/CustomerForm';
+import AgentResponseForm from './components/AgentResponseForm';
 
 function App() {
+  const [selectedMessage, setSelectedMessage] = useState(null);
+
   return (
-    <div className="App">
-      <h1>Messaging App</h1>
-      <CustomerForm />
-      {/* <CustomerMessageForm /> */}
-      <AgentMessageList />
-      
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Messaging App</h1>
+        <Routes>
+          <Route path="/" element={<CustomerForm />} />
+          <Route path="/agent" element={
+            selectedMessage ? (
+              <AgentResponseForm message_id={selectedMessage.id} agent_id={1} />
+            ) : (
+              <AgentMessageList setSelectedMessage={setSelectedMessage} />
+            )
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
+
 export default App;
+
+// // src/App.js
+// import React from 'react';
+// import './App.css';
+// import CustomerMessageForm from './components/CustomerMessageForm';
+// import AgentMessageList from './components/AgentMessageList';
+// import CustomerForm from './components/CustomerForm';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Messaging App</h1>
+//       <CustomerForm />
+//       {/* <CustomerMessageForm /> */}
+//       <AgentMessageList />
+      
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // import logo from './logo.svg';
 // import './App.css';
